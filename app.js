dbd1fc1a97921d95bc12bd76873695c766a0e8ad
Jonathan Otano <jonathanotano@icloud.com>
2017-10-16 19:14:25 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+// sync user preference of timer with displayed timer
+var timer = document.getElementById("timer");
+var timerLength = document.getElementById("timerLength");
+
+timer.innerHTML = timerLength.innerHTML;
+
+// add functionality to plus and minus buttons
+
+// add timer/countdown function
+
+// start "rest" timer when countdown finishes
